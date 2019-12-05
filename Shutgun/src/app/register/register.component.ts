002cd6b25915b8bc9a7c687d06a41448e443b168
erik <erikpilverdier1@gmail.com>
2019-12-05 11:06:36 +0100

    registerform

--- Shutgun/src/app/register/register.component.ts
+++ Shutgun/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm; FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.registerForm = this.fb.group({
+      "firstName": ['', Validators.required],
+      "lastName": ['', Validators.required],
+      "email": ['', [Validators.required, Validators.email]],
+      "password": ['', Validators.required, Validators.minLength(6)],
+      "city": ['', Validators.required],
+      "birthDate": ['', Validators.required],
+      "phoneNumber": ['', Validators.required, Validators.minLength(8)]
+    })
   }
 
+  onRegisterClick(): void {
+    console.log(this.registerForm);
+    // if this form is valid - then call the server
+    if (this.registerForm.valid) {
+      // call server
+    }
+    else{
+      // ?
+    }
+  }
 }
